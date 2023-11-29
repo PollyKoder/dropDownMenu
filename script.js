f022767e61e643de82450dbe6853dc53164dcb7a
Polly <riviera.koder@gmail.com>
2023-11-29 14:49:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Hent knappen og ul
+//hent navbar i knapp function? 
+//Tips tenk classList.toggle og classList.add/remove, mellom navbarHidden og navbarShow.
+
+const listMenu = document.querySelector("#menuUl");
+const burgerMenu = document.querySelector("#menuBurger");
+
