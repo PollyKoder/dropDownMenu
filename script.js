bcbfac6641ca858cd576348e341b69fd4e3e6aa5
Polly <riviera.koder@gmail.com>
2023-11-30 11:30:56 +0100

    edit_functionAndCSS

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //Hent knappen og ul
-//hent navbar i knapp function? 
-//Tips tenk classList.toggle og classList.add/remove, mellom navbarHidden og navbarShow.
-
 const listMenu = document.querySelector("#menuUl");
 const burgerMenu = document.querySelector("#menuBurger");
+
+//hent navbar i knapp function? 
+//Tips tenk classList.toggle og classList.add/remove, mellom navbarHidden og navbarShow.
+function showNav() {
+    listMenu.classList.toggle("navbarHidden");
+};
+
+burgerMenu.addEventListener("click", showNav);
 
